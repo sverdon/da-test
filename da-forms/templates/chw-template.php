ecd60887ce57cd83e6f586acda3b19831fcd9c2e
Spencer Verdon <verdon.spencer@gmail.com>
2023-05-12 13:38:48 -0600

    try new tm adder file

--- da-forms/templates/chw-template.php
+++ da-forms/templates/chw-template.php
@@ -46,7 +46,7 @@ $filename = 'CHWAdder_' . rtrim($filename, '_') . '.xlsx';
 $filepath = 'chw-templates/' . $filename;
 
 // Load Template
-$template = 'chw_template.xlsx';
+$template = 'NEW-Team_Member_Adder_CHW.xlsx';
 $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
 $spreadsheet = $reader->load($template);
 
